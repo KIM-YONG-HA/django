80ccb0cd1812c3ea63f90619cf426d164ccf071b
KIM-YONG-HA <vb901217@gmail.com>
2025-01-07 14:14:34 +0900

    250107 조회와템플릿, url 별칭 md 작성

--- projects/mysite/config/urls.py
+++ projects/mysite/config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from pybo import views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("pybo/", views.index),
+    #path("pybo/", views.index),
+    #path('pybo/<int:question_id>/', views.detail)
+    path('pybo/', include('pybo.urls'))
 ]
