1910b71f2bfcb201dbbde6e886f3e1d4c3b62803
KIM-YONG-HA <vb901217@gmail.com>
2025-01-23 15:21:03 +0900

    250123 회원가입 진행

--- projects/mysite/common/urls.py
+++ projects/mysite/common/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
     #  django.contrib.auth 앱의 LoginView를 사용하도록 설정 
     path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
