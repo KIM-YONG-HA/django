b135cffe3558af05c58cea63c3bc519236ab1190
KIM-YONG-HA <vb901217@gmail.com>
2025-01-24 13:56:46 +0900

    250124 게시판 모델 변경

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,13 +1,11 @@
 #from django.http import HttpResponse
-from django.http import HttpResponseNotAllowed
+# from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-
 from .models import Question
 from .forms import QuestionForm, AnswerForm
-
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
 
 # def index(request):
 #     return HttpResponse("hello django!")
@@ -37,23 +35,29 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
-def answer_create(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+
+# def answer_create(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     
-    # import .modesl Question, Answer 추가 필요 
-    #answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    #answer.save()
+#     # import .modesl Question, Answer 추가 필요 
+#     #answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+#     #answer.save()
     
-    return redirect('pybo:detail', question_id=question_id)
+#     return redirect('pybo:detail', question_id=question_id)
 
 
+
+
+
+
+@login_required(login_url='common:login')
 def question_create(request):
-
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
@@ -64,24 +68,42 @@ def question_create(request):
     return render(request, 'pybo/question_form.html' , context)
 
 
+# def answer_create(request, question_id):
+#     """
+#     pybo 답변 등록
+#     """
+#     question = get_object_or_404(Question, pk=question_id)
+#     if request.method == 'POST':
+#         form = AnswerForm(request.POST)
+#         if form.is_valid():
+#             answer = form.save(commit=False)
+#             answer.author = request.user
+#             answer.create_date = timezone.now()
+#             answer.question = question
+#             answer.save()
+#             return redirect('pybo:detail', question_id=question.id)
+#     else:
+#         return HttpResponseNotAllowed('Only POST is possible.')
+    
+#     context = {'question': question, 'form': form}
+#     return render(request, 'pybo/question_detail.html', context)
+
+
+
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
-    """
-    pybo 답변 등록
-    """
     question = get_object_or_404(Question, pk=question_id)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question.id)
+            return redirect('pybo:detail', question_id=question_id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
-    
+        form = AnswerForm()
+
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
-
-
-
