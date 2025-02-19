9155bbd0a0dbd07efacc0cba1338165c2561c431
KIM-YONG-HA <vb901217@gmail.com>
2025-02-19 14:15:59 +0900

    250219 댓글 추천기능 추가

--- projects/mysite/pybo/views/answer_views.py
+++ projects/mysite/pybo/views/answer_views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
+from django.http import HttpResponse
 
 from ..forms import AnswerForm
 from ..models import Question, Answer
@@ -18,7 +19,10 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo:detail', question_id=question_id)
+            # return redirect('pybo:detail', question_id=question_id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=question.id), answer.id
+            ))
     else:
         form = AnswerForm()
 
@@ -39,7 +43,10 @@ def answer_modify(request, answer_id):
             answer = form.save(commit=False)
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pybo:detail', question_id=answer.question.id)
+            return redirect('{}#answer_{}'.format(
+                resolve_url('pybo:detail', question_id=answer.question.id), answer.id
+            ))
+            # return redirect('pybo:detail', question_id=answer.question.id)
     else:
         form = AnswerForm(instance=answer)
     
@@ -56,3 +63,21 @@ def answer_delete(request, answer_id):
         answer.delete()
     return redirect('pybo:detail', question_id=answer.question.id)
     
+
+@login_required(login_url='common:login')
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, id=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 댓글은 추천할 수 없습니다.')
+    else: 
+        answer.voter.add(request.user)
+        messages.success(request, '추천이 완료되었습니다.')
+
+    return redirect('pybo:detail', question_id=answer.question.id)
+
+    #return HttpResponse('test')
+
+    #return redirect('pybo:detail', question_id=answer.question.id)
+    # return redirect('{}#answer_id{}'.format(
+    #     resolve_url('pybo:detail')
+    # ))
