64f0dccd3edce4535d388b1fce35142cfb9a6ba1
KIM-YONG-HA <vb901217@gmail.com>
2025-03-27 17:52:14 +0900

    250327 maumcheck django 메인 수정 및 이미지 추가

--- projects/maumcheck/counsel/models.py
+++ projects/maumcheck/counsel/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CounselQuestion(models.Model):
+    subject = models.CharField(max_length=200)
+    name = models.CharField(max_length=10)
+    age = models.CharField(max_length=10)
+    address = models.CharField(max_length=10)
+    content = models.TextField()
+
+    emotion = models.TextField()
+    situation = models.TextField()
+    hashtag = models.CharField(max_length=10)
+    
+    create_date = models.DateTimeField()
